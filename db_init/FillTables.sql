aba985fe6078c4f092d40d2e01fd4667098f6530
KWojcik243 <owcaaa243@gmail.com>
2023-05-21 18:42:23 +0200

    ADD: files to create tables and insert data

---
+++ db_init/FillTables.sql
@@ -0,0 +1,8 @@
+INSERT INTO "Books" 
+VALUES ("Władca Pierścieni: Bractwo Pierścienia","./book_cover/Wladca_Pierscieni_Bractwo Pierscienia","12","8.7","9788328724624","1","255");
+
+INSERT INTO "Authors"
+VALUES ("J.R.R.","Tolkien");
+
+INSERT INTO "BookCategories"
+VALUES ("1","Fantasy");
