162ccd0c1a47391dded52d5af01eddb22a2d9bbb
KWojcik243 <63014344+KWojcik243@users.noreply.github.com>
2023-05-21 19:11:38 +0200

    ADD: working initialize db. FIX: database initialize properly

--- db_init/FillTables.sql
+++ db_init/FillTables.sql
@@ -1,8 +1,8 @@
-INSERT INTO "Books" 
-VALUES ("Władca Pierścieni: Bractwo Pierścienia","./book_cover/Wladca_Pierscieni_Bractwo Pierscienia","12","8.7","9788328724624","1","255");
+INSERT INTO "Authors" (name, surname)
+VALUES ('J.R.R.', 'Tolkien');
 
-INSERT INTO "Authors"
-VALUES ("J.R.R.","Tolkien");
+INSERT INTO "Books" (title, png_path, age_group, rating, isbn, author_id, amount)
+VALUES ('Władca Pierścieni: Bractwo Pierścienia','./book_cover/Wladca_Pierscieni_Bractwo Pierscienia','12','8.7','9788328724624','1','255');
 
-INSERT INTO "BookCategories"
-VALUES ("1","Fantasy");
+INSERT INTO "BookCategories" (book_id, category)
+VALUES ('1','Fantasy');
