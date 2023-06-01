1fe4e4ccfbb7c5998fd9900c7df740d1e0dd956d
Pawel Nowak <pawelnow12@gmail.com>
2023-06-01 11:45:25 +0200

    fix names in order controller

--- src/main/java/com/example/demo/controller/OrderController.java
+++ src/main/java/com/example/demo/controller/OrderController.java
@@ -16,22 +16,22 @@ public class OrderController {
     OrderService orderService;
 
     @Autowired
-    public OrderController(OrderService orderService){
+    public OrderController(OrderService orderService) {
         this.orderService = orderService;
     }
 
-    @GetMapping("/ordersByUser")
-    public List<Order> getOrdersByUserId(@RequestParam int id){
+    @GetMapping("/getOrdersByUser")
+    public List<Order> getOrdersByUserId(@RequestParam int id) {
         return orderService.getOrdersByUserId(id);
     }
 
-    @PostMapping("/order")
+    @PostMapping("/addOrder")
     public void addOrder(@RequestParam Timestamp lastStatusUpdate,
                          @RequestParam String status,
                          @RequestParam int paymentType,
                          @RequestParam String address,
                          @RequestParam String city,
-                         @RequestParam String postCode){
+                         @RequestParam String postCode) {
         this.orderService.addOrder(lastStatusUpdate, status, paymentType, address, city, postCode);
     }
 
